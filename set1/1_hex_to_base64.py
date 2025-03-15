b261115db649da674518b7cd89d6405faf7259fe
madeeha <m4deeh4@gmail.com>
2025-03-15 13:16:29 -0400

    fix comments

--- set1/1_hex_to_base64.py
+++ set1/1_hex_to_base64.py
@@ -4,12 +4,12 @@ import base64
 # hex string: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
 # expected output: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t 
 
-# collect hex string to convert from user
+# collect hex string from user
 hex_string = input("Please enter your hex string: ")
 
 # perform encoding operation on raw bytes
 hex_bytes = bytes.fromhex(hex_string)
 encoded_data = base64.b64encode(hex_bytes)
 
-# convert encoded bytes back to encoded string bfore outputing because it looks better
+# convert encoded bytes back to encoded string because it looks better
 print(encoded_data.decode("utf-8"))
